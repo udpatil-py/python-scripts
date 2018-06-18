5e16bf2de2a1a542c1db87b1e51a2e87e142c009
UDAY KUMAR PATIL <39081150+udpatil-py@users.noreply.github.com>
2018-06-18 07:39:46 -0700

    find files and file sizes recursively
    various scenarios and functions used to achieve same task , just for fun

---
+++ README.txt
@@ -0,0 +1,82 @@
+Hello,
+
++----------------+
+| Project Title: |											|
++----------------+
+List out all the files on the mountpoint and their disk usage in bytes in json format.
+
+This program is written in python 3.5.4 on linux machine RHEL 6 and tested on few cases to my best.
+
+Assuming there will be some generic user id and password is provided to developers to perform
+certain tasks with limited access.
+
+In this case I have assumed that the developer is allowed to enter the password,however the script just connect the
+remote server on specific given mount path and performs required task and umount it immidiately once results are
+written to the getdiskusage.log file.
+
+The script is written based on 4 scenarios,considering sshfs
+each scenario shows its performace and can be found in the logs.
+1. using glob function recursively
+2. using normal os module with simple option recursively
+3. using os.walk
+4. using glob function recursively, but given a threshold and timeout scenarios which may be helpful when we touch huge prod filesystems
+so,the program will stop fetcthin the file information once the timeout crosses to avoid any extra load on the production landing servers
+and it is always welcomes any recommendation or changes if required in real time, like size limit and timeout limits.
+
+
++--------------------------------------+
+| Configuration details/Prerequisites: |											|
++--------------------------------------+
+My system Arch:
+Linux localhost.localdomain 2.6.32-71.el6.i686 #1 SMP Wed Sep 1 01:26:34 EDT 2010 i686 i686 i386 GNU/Linux
+
+Install python 3.5.4
+https://www.python.org/ftp/python/3.5.4/Python-3.5.4.tar.xz
+
+install and configure two linux servers,one to run the code another as a remote servers to access its filesystem
+install sshfs on both 
+wget https://rpmfind.net/linux/epel/6/i386/Packages/f/fuse-sshfs-2.4-1.el6.i686.rpm
+rpm -ivh fuse-sshfs-2.4-1.el6.i686.rpm
+
+configure sudo
+visudo
+%wheel ALL=(ALL)       NOPASSWD: ALL #uncomment
+usermod -aG wheel python
+
+copy the code getdiskusage_version_1.0.0.py 
+
+
+Input Test cases,few are given below:
+python3 getdiskusage_version_1.0.0.py rhel6@192.168.84.135:/
+python3 getdiskusage_version_1.0.0.py rhel6@192.168.84.135:/root
+python3 getdiskusage_version_1.0.0.py rhel6@192.168.84.135:/root/
+python3 getdiskusage_version_1.0.0.py rhel6@192.168.84.135://////
+python3 getdiskusage_version_1.0.0.py test@servername:/opt/target/monitor/app
+python3 getdiskusage_version_1.0.0.py 192.168.84.135:/home/rhel6/
+python3 getdiskusage_version_1.0.0.py rhel6@192.168.84.135:/home/rhel6/Desktop/target/
+python3 getdiskusage_version_1.0.0.py rhel6@192.168.84.135:/home/rhel6/Desktop/target/monitor
+python3 getdiskusage_version_1.0.0.py rhel6@192.168.84.135:/home/rhel6/Desktop/target/monitor/logs
+
+
+sample_command = 'sudo sshfs -o allow_other,IdentityFile=~/.ssh/id_rsa python@192.168.84.135:/home/python/Desktop/python_scripts /home/rhel6/Desktop/remote1'
+
+sudo fusermount -u /home/python/Desktop/newmount
+
+#create some junk files with random system files on remote server,that is server two
+import os
+import time
+import random
+
+flist = ['/proc/cpuinfo','/proc/meminfo','/proc/crypto','/proc/uptime','/proc/swaps','/proc/zoneinfo']
+for x in range(1000):
+    fn = str(time.time())+".log"
+    f = random.choice(flist)
+    os.system("cat "+f+" > "+fn)
+
+
++---------+
+| AUTHOR: |											|
++---------+
+UDAY KUMAR PATIL
+udpatil.py@gmail.com
+https://github.com/udpatil-py/python-scripts
---
+++ getdiskusage_version_1.0.0.py
@@ -0,0 +1,276 @@
+#!/usr/local/bin/python3
+###########################################################################
+#  Script     : getdiskusage_version_1.0.0.py                             #
+#  Version    : Python 3.5.4                                              #
+#  Author     : Uday Kumar Patil  (udpatil.py@gmail.com)                  #
+#  Date       : 06/16/2018                                                #
+#  Description: Script to find files recursively on given path.           #
+###########################################################################
+
+#----------------------------------------------------------------------------------------------------#
+import os
+import logging
+import sys
+import glob
+import re
+import json
+import time
+import subprocess
+
+#----------------------------------------------------------------------------------------------------#
+#Assumptions and extra comments about the program
+#I assume that as per condition, SSH is not allowed without VP approval
+#hence there should be a sudo access,to mount the remote filesystem on local server
+#or we should be allowed to mount the path of interest with generic usernames and passwords if aggreed
+#----------------------------------------------------------------------------------------------------#
+#initializing global variables
+path = sys.argv[1]
+dirs = []
+rfilex = []
+path_check = re.compile(r"[^.\-\w\/\:\@]")
+LOG_FORMAT = "%(levelname)s - %(asctime)s - %(message)s"
+logging.basicConfig(filename="getdiskusage.log",filemode="a",level=logging.DEBUG,format=LOG_FORMAT)
+logger = logging.getLogger()
+#----------------------------------------------------------------------------------------------------#
+
+
+#----------------------------------------------------------------------------------------------------#            
+def main(path):
+   """main program starts here,check path format and proceed"""
+   logger.info("checking path format")
+   while path_check.search(path):
+       print("Invalid path syntax...!")
+       logger.error("Invalid path syntax...!",path)
+       logger.info("waiting for user to enter correct path")
+       path = input("Please enter absolute path name to proceed...!\nExample:'username@server:/opt/target/monitor/app/' \n")
+
+   logger.info("given path has all expected characters and checking further now")
+   
+   status, newpath = server_check_and_mount(path)
+   
+   if status:
+       logger.info("path is valid and mounted successfully on local server")
+       
+       for testfun in [search_files_1,search_files_2,search_files_3,search_files_4]:
+           time1 = time.time()
+           if testfun != search_files_2:
+               data = testfun(newpath)
+               time2 = time.time()
+           else:
+               testfun(newpath)
+               if dirs != []:
+                   for cd in dirs:
+                       list_files(cd)
+               else:
+                   list_files(newpath)
+               time2 = time.time()
+               data = rfilex
+           if data is not False and data != []:
+               json_data = json.dumps({"files":data})
+               #print("\n\n",json_data)
+               logger.info("number of files found using %s function is %s:"%(testfun,str(len(data))))
+               logger.info("%s function took %0.3f ms"%(testfun,(time2-time1)*1000.0))
+               logger.info(20*"="+"Just displaying only few top and bottom files of json data"+20*"=")
+               logger.info("\n,\n"+json_data[0:1000]+5*"\n."+json_data[-1000:])
+               logger.info(100*"*")
+               logger.info(40*"*"+"Next Function Starts"+40*"*")
+               logger.info(100*"*")
+           else:
+               logger.info(24*"="+"No files found"+24*"=")
+               print('\n'+24*"-"+"Please check getdiskusage.log for more information"+24*"-")
+       
+       logger.info("Waiting to unmount the filesystem as required task is completed") 
+       time.sleep(5)
+       command = "sudo fusermount -u "+newpath
+       status = os.system(command)
+       if status == 0:
+           logger.info("successfully unmounted the remote filesystem from local machine")
+           print("successfully unmounted the remote filesystem from local machine")
+       else:
+           logger.error("unable to umount the path"+newpath)
+           print("unable to umount path",newpath)
+
+#----------------------------------------------------------------------------------------------------#
+def server_check_and_mount(path):
+    
+    try:
+        if "@" in path and ":" in path:
+            username,serverip,mountpath = re.split("@|:",path)
+            command = "sudo sshfs -o allow_other,IdentityFile=~/.ssh/id_rsa {0}@{1}:{2} /home/python/Desktop/newmount".format(username,serverip,mountpath)
+            logger.info("username:"+username)
+            print("UserName:",username)
+        elif "@" not in path and ":" in path:
+            serverip,mountpath = re.split(":",path)
+            command = "sudo sshfs -o allow_other,IdentityFile=~/.ssh/id_rsa {0}:{1} /home/python/Desktop/newmount".format(serverip,mountpath)
+        else:
+            logger.info("incorrect path format received,desired format is : username@server@/path")
+            print("Please enter path in desired format example: username@server:/path")
+            sys.exit()
+        logger.info("ServerName/ip:"+serverip)
+        logger.info("Remote Server mountpath:"+mountpath)
+        print("ServerName/ip:",serverip)
+        print("Remote Server mountpath:",mountpath)
+
+        status = os.system("ping -c 1 "+serverip)
+
+        if status != 0:
+            logger.error("Server not reachable")
+            sys.exit("ERROR : Server not reachable")
+        elif status == 0:
+            mountpoints = subprocess.check_output('mount')
+	
+            if len(list(set(mountpath))) == 1:
+                logger.error("Invalid input path"+mountpath)
+                sys.exit("Invalid input path")
+
+            if mountpath == "/" or  mountpath == "/root" or mountpath == "/root/":
+                logger.warning("mount is restricted on root directories, exiting the program")
+                sys.exit("mount is restricted on root directories")
+            elif mountpath in str(mountpoints):
+                logger.info(10*"-"+"path is already in mount,please check below mount locations"+10*"-"+"\n"+mountpoints)
+                print(10*"-"+"path is already in mount,please check below mount locations"+10*"-"+"\n\n"+mountpoints)
+                sys.exit()
+            else:
+                status = os.system(command)
+                if status == 0:
+                    newpath = "/home/python/Desktop/newmount"
+                    logger.info("newmount path is received now:"+newpath)
+                    return (True,newpath)
+                else:
+                    logger.error("Unable to mount")
+                    print("Unable to mount")
+        else:
+            logger.error("Invalid Server Name")
+            sys.exit("Invalid Server Name")
+    except Exception as e:
+        logger.error("Exception occurred in {0} function \n {1}".format(server_check_and_mount.__name__,e))
+        print("Exception occured ",e)
+
+#----------------------------------------------------------------------------------------------------#
+
+    
+#----------------------------------------------------------------------------------------------------#
+def search_files_1(path):
+    """search file recursively for given path"""
+    logger.info("Started to search file recursively using glob function")
+    filex = []
+    try:
+        for file in glob.glob(path+"/**/*",recursive=True):
+            file_name = os.path.join(path,file)
+            if os.path.isfile(file_name):
+                size = os.path.getsize(file)
+                temp = {file:size}
+                filex.append(temp)
+        return filex
+    except Exception as e:
+        logger.error("Exception occurred in {0} function \n {1}".format(search_files_1.__name__,e))
+        print("Exception occurred in {0} function \n {1}".format(search_files_1.__name__,e))
+        return filex
+#----------------------------------------------------------------------------------------------------#
+
+
+#----------------------------------------------------------------------------------------------------#
+"""search file recursively and return search result without using os.walk"""
+def search_files_2(path):
+    "find all directory names with full path"
+    try:
+        files = os.listdir(path)
+        for file in files:
+            x = os.path.join(path,file)
+            if os.path.isdir(x):
+                dirs.append(x)
+                search_files_2(x)
+    except Exception as e:
+        logger.error("Exception occurred in {0} function \n {1}".format(search_files_2.__name__,e))
+        print("Exception occurred in {0} function \n {1}".format(search_files_2.__name__,e))
+ 
+def list_files(cd):
+    "list file names and size of the given directory"
+    try:
+        files = os.listdir(cd)
+        for file in files:
+            file_name = os.path.join(cd,file)
+            if os.path.isfile(file_name):
+                size = os.path.getsize(file_name)
+                rfilex.append({file_name:size})
+    except Exception as e:
+        logger.error("Exception occurred in {0} function \n {1}".format(list_files.__name__,e))
+        print("Exception occurred in {0} function \n {1}".format(list_files.__name__,e))
+        
+#----------------------------------------------------------------------------------------------------#
+
+
+#----------------------------------------------------------------------------------------------------#
+def search_files_3(path):
+    """search files using os.walk"""
+    filex = []
+    logger.info("started finding files using os.walk")
+    try:
+        for dirpath,dirname,filelist in os.walk(path):
+            for file in filelist:
+                file_name = os.path.join(dirpath,file)
+                if os.path.isfile(file_name):
+                    size = os.path.getsize(file_name)
+                    filex.append({file_name:size})
+        return filex
+    except Exception as e:
+        logger.error("Exception occurred in {0} function \n {1}".format(search_files_3.__name__,e))
+        print("Exception occurred in {0} function \n {1}".format(search_files_3.__name__,e))
+        return filex
+#----------------------------------------------------------------------------------------------------#
+
+
+#----------------------------------------------------------------------------------------------------#
+def search_files_4(path):
+    """search files above given threshold size recursively,
+    and stop search if timeout and return search result"""
+    
+    logger.info("finding logs which are more than 100MB in size with in 30 seconds of given timeout")
+    filex = []
+    "below threshold set to find files more than 100 MB"
+    threshold_size = 100000000 #100MB, 100 followed by 6 0's
+    
+    "30 seconds time out threshold to stop search"
+    timeout_threshold = 30 #default 30 sec maximum search time given
+    try:
+        timeout = time.time()+timeout_threshold
+        while timeout > time.time():
+            logger.info("started file search with file size limit of 100MB and timeout of 30 seconds at:Remote Server mountpath:\t"+path)
+            time.asctime()
+#            print("\nstarted file search with file size limit of 100MB and timeout of 30 seconds at:",time.asctime())
+            for n in range(1):
+                for file in glob.glob(path+"/**/*",recursive=True):
+                    size = os.path.getsize(file)
+                    if size > threshold_size:
+                        temp = {file:size}
+                        filex.append(temp)
+            break
+        
+        if timeout < time.time():
+            logger.warning("file search stopped due to timeout,default timeout is 30 sec:"+time.asctime())
+#            print("file search stopped due to timeout,default timeout is 30 sec:",time.asctime())
+        else:
+            logger.info("file search completed by:"+time.asctime())
+#            print("file search completed by:",time.asctime())
+        
+        if filex == []:
+            logger.info("no files found more than 100MB in size")
+            return filex
+        else:
+            return filex
+    
+    except Exception as e:
+        logger.error("Exception occurred in {0} function \n {1}".format(search_files_4.__name__,e))
+        print("Exception occurred in {0} function \n {1}".format(search_files_4.__name__,e))
+        return filex
+#----------------------------------------------------------------------------------------------------#
+
+
+#-------------------------------------------START----------------------------------------------------#
+if __name__ == "__main__":
+    logger.info(24*"="+"Program Started"+24*"=")
+    print(24*"="+"Program Started"+24*"=")
+    main(path)
+    logger.info(20*"="+"Program Ended successfully"+20*"=")
+    print(20*"="+"Program Ended successfully"+20*"=")
+#--------------------------------------------END-----------------------------------------------------#
